71741ea32a0a52f73756a0de7b708a30cc319f8d
sudonymIO <sudhanshul@yahoo.com>
2020-05-08 16:22:59 -0400

    Added exercise #4

--- exercises.py
+++ exercises.py
@@ -1,11 +1,12 @@
 import datetime
 import time
+import re
 
 #1 Print the time
 print("*"*20,"Exercise 1","*"*20)
 print(datetime.datetime.now())
 
-print("*"*20)
+
 print("*"*20,"Exercise 2","*"*20)
 #2 - Simple stop watch
 timeStart = datetime.datetime.now()
@@ -20,17 +21,45 @@ print("*"*20,"Exercise 3","*"*20)
 value = input("Please type your name: ")
 print(f"Hi {value}")
 
+print("*"*20,"Exercise 4","*"*20)
+inputFromUser = ""
+def hasNumers(word):
+    match = re.search(r"[0-9_].", word)
+    if match:
+        return True
+    else:
+        return False 
+
+def askForUserInput():
+    inputFromUser = input("Enter a word:")
+    wordHasNumbers = hasNumers(inputFromUser)
+    if wordHasNumbers:
+        print(f"{inputFromUser} is not a word.")
+        ui = input("Do you wan to try again:")
+        if ui == "y":
+            askForUserInput()
+        else:
+            pass
+    else:
+        print("Great, it's a word")
+
+
+askForUserInput()
+
 
 print("*"*20,"Exercise 5","*"*20)
 # Exercise 5: Record and Print a List
 inputList = []
-for i in range(3):
-    val = input("Type a number:")
+for i in range(3,0,-1):
+    val = input(f"Type a number, we will have you {i-1} more time:")
     inputList.append(val)
 
 print(inputList)
 
 
+
+# ********************************************************************************************
+# *This is not required but I started with this and got me thinking that this would be good one to solve for. I will come back to this later*
 
 # #welcome message
 # print("welcome to the nifty stopwatch build on Python")
